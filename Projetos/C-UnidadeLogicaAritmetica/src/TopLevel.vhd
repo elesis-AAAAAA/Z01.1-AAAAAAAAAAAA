589a3ce419f46ebc4aea5a7b5267eb0f414bb2fa
Andre Brito <felipe.brito016@gmail.com>
2022-03-23 14:47:17 -0300

    try fpga port map

--- Projetos/C-UnidadeLogicaAritmetica/src/TopLevel.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/TopLevel.vhd
@@ -19,7 +19,13 @@ use ieee.numeric_std.all;
 entity TopLevel is
 	port(
 		SW      : in  std_logic_vector(9 downto 0);
-		LEDR    : out std_logic_vector(9 downto 0)
+		LEDR     : out std_logic_vector(9 downto 0);
+		HEX0		: out std_logic_vector(6 downto 0);
+		HEX1 : out std_logic_vector(6 downto 0);
+		HEX2 : out std_logic_vector(6 downto 0);
+		HEX3 : out std_logic_vector(6 downto 0);
+		HEX4 : out std_logic_vector(6 downto 0);
+		HEX5 : out std_logic_vector(6 downto 0)
 	);
 end entity;
 
@@ -34,7 +40,7 @@ architecture rtl of TopLevel is
 
   signal x : std_logic_vector(15 downto 0) := x"0073"; -- 115
   signal y : std_logic_vector(15 downto 0) := x"005F"; -- 95
-
+  signal bcd1 : std_logic_vector(3 downto 0);
 --------------
 -- component
 --------------
@@ -52,11 +58,54 @@ architecture rtl of TopLevel is
           );
     end component;
 
+  component ALU is
+    port (
+			x,y:   in STD_LOGIC_VECTOR(15 downto 0); -- entradas de dados da ALU
+			zx:    in STD_LOGIC;                     -- zera a entrada x
+			nx:    in STD_LOGIC;                     -- inverte a entrada x
+			zy:    in STD_LOGIC;                     -- zera a entrada y
+			ny:    in STD_LOGIC;                     -- inverte a entrada y
+			f:     in STD_LOGIC;                     -- se 0 calcula x & y, senão x + y
+			no:    in STD_LOGIC;                     -- inverte o valor da saída
+			zr:    out STD_LOGIC;                    -- setado se saída igual a zero
+			ng:    out STD_LOGIC;                    -- setado se saída é negativa
+			saida: out STD_LOGIC_VECTOR(15 downto 0) -- saída de dados da ALU
+	);
+
+  end component;
+
+
+  component sevenSeg is
+		port (
+				bcd : in  STD_LOGIC_VECTOR(3 downto 0);
+				leds: out STD_LOGIC_VECTOR(6 downto 0));
+	end component;
+
 ---------------
 -- implementacao
 ---------------
 begin
 
-  u1 : HalfAdder port map(a => SW(0), b=> SW(1), soma => LEDR(0), vaium => LEDR(1));
+  -- u1 : HalfAdder port map(a => SW(0), b=> SW(1), soma => LEDR(0), vaium => vaiumHalf(1));
+  -- u2 : FullAdder port map(a => SW(2), b => SW(3), c => vaiumHalf(4), soma => LEDR(2), vaium => LEDR(3));
 
+  u1 : ALU port map(
+    x => x,
+    y => y,
+    zx => SW(0),
+    nx => SW(1),
+    zy => SW(2),
+    ny => SW(3),
+    f => SW(4),
+    no => SW(5),
+    zr => LEDR(0),
+    ng => LEDR(1),
+    saida(3 downto 0) => bcd1
+    
+  );
+
+  hex0port: sevenSeg port map (
+			bcd => bcd1,
+			leds => HEX0);
+		
 end rtl;
