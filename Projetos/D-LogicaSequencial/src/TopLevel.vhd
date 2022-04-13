d65b7d295b79094a73cf3d52a30431424f061496
Andre Brito <felipe.brito016@gmail.com>
2022-04-13 15:31:35 -0300

    PC on FPGA

--- Projetos/D-LogicaSequencial/src/TopLevel.vhd
+++ Projetos/D-LogicaSequencial/src/TopLevel.vhd
@@ -7,140 +7,69 @@ use ieee.std_logic_1164.all;
 
 entity TopLevel is
 	port(
-		clock:   in  STD_LOGIC;
-		input:   in  STD_LOGIC_VECTOR(15 downto 0);
-		load:    in  STD_LOGIC;
-		address: in  STD_LOGIC_VECTOR(2 downto 0);
-		output:  out STD_LOGIC_VECTOR(15 downto 0)
+		SW      : in  std_logic_vector(9 downto 0);
+		LEDR     : out std_logic_vector(9 downto 0);
+		KEY		 : in  std_logic_vector(3 downto 0);
+		HEX0		: out std_logic_vector(6 downto 0);
+		HEX1 : out std_logic_vector(6 downto 0);
+		HEX2 : out std_logic_vector(6 downto 0);
+		HEX3 : out std_logic_vector(6 downto 0)
 	);
 end entity;
 
+
+
 architecture arch of TopLevel is
 
-	component Register16 is
-		port(
-			clock:   in  STD_LOGIC;
-			input:   in  STD_LOGIC_VECTOR(15 downto 0);
-			load:    in  STD_LOGIC;
-			output:  out STD_LOGIC_VECTOR(15 downto 0)
-		);
-	end component;
+signal output_pc : std_logic_vector(15 downto 0);
+signal input_pc : std_logic_vector(15 downto 0) := "0000000000000000";
 
-	component Mux8Way16 is
-		port (
-				a:   in  STD_LOGIC_VECTOR(15 downto 0);
-				b:   in  STD_LOGIC_VECTOR(15 downto 0);
-				c:   in  STD_LOGIC_VECTOR(15 downto 0);
-				d:   in  STD_LOGIC_VECTOR(15 downto 0);
-				e:   in  STD_LOGIC_VECTOR(15 downto 0);
-				f:   in  STD_LOGIC_VECTOR(15 downto 0);
-				g:   in  STD_LOGIC_VECTOR(15 downto 0);
-				h:   in  STD_LOGIC_VECTOR(15 downto 0);
-				sel: in  STD_LOGIC_VECTOR(2 downto 0);
-				q:   out STD_LOGIC_VECTOR(15 downto 0));
-	end component;
+component PC is
+	port(
+			clock     : in  STD_LOGIC;
+			increment : in  STD_LOGIC;
+			load      : in  STD_LOGIC;
+			reset     : in  STD_LOGIC;
+			input     : in  STD_LOGIC_VECTOR(15 downto 0);
+			output    : out STD_LOGIC_VECTOR(15 downto 0)
+	);
+end component;
 
-	component DMux8Way is
-		port (
-			a:   in  STD_LOGIC;
-			sel: in  STD_LOGIC_VECTOR(2 downto 0);
-			q0:  out STD_LOGIC;
-			q1:  out STD_LOGIC;
-			q2:  out STD_LOGIC;
-			q3:  out STD_LOGIC;
-			q4:  out STD_LOGIC;
-			q5:  out STD_LOGIC;
-			q6:  out STD_LOGIC;
-			q7:  out STD_LOGIC
-		);
-	end component;
-
-	signal load0, load1, load2, load3, load4, load5, load6, load7 : STD_LOGIC;
-	signal output0, output1, output2, output3, output4, output5, output6, output7 : STD_LOGIC_VECTOR(15 downto 0);
+component sevenSeg is
+	port (
+			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
+			leds: out STD_LOGIC_VECTOR(6 downto 0));
+end component;
 
 begin
 
-	dMuxPm: DMux8Way port map(
-		load,
-		address,
-		load0,
-		load1,
-		load2,
-		load3,
-		load4,
-		load5,
-		load6,
-		load7
+
+	pcpm: PC port map(
+		clock => not KEY(0),
+		increment => SW(0),
+		load => SW(1),
+		reset => SW(2),
+		input => input_pc,
+		output => output_pc
 	);
 
-	reg0Pm: Register16 port map(
-		clock,
-		input,
-		load0,
-		output0
-	);
+	hex0port: sevenSeg port map (
+		bcd => output_pc(3 downto 0),
+		leds => HEX0);
 
-	reg1Pm: Register16 port map(
-		clock,
-		input,
-		load1,
-		output1
-	);
+	hex1port: sevenSeg port map (
+		bcd => output_pc(7 downto 4),
+		leds => HEX1);
+	
+	hex2port: sevenSeg port map (
+		bcd => output_pc(11 downto 8),
+		leds => HEX2);
+	
+	hex3port: sevenSeg port map (
+		bcd => output_pc(15 downto 12),
+		leds => HEX3);
 
-	reg2Pm: Register16 port map(
-		clock,
-		input,
-		load2,
-		output2
-	);
 
-	reg3Pm: Register16 port map(
-		clock,
-		input,
-		load3,
-		output3
-	);
-
-	reg4Pm: Register16 port map(
-		clock,
-		input,
-		load4,
-		output4
-	);
-
-	reg5Pm: Register16 port map(
-		clock,
-		input,
-		load5,
-		output5
-	);
-
-	reg6Pm: Register16 port map(
-		clock,
-		input,
-		load6,
-		output6
-	);
-
-	reg7Pm: Register16 port map(
-		clock,
-		input,
-		load7,
-		output7
-	);
-
-	mux8Pm: Mux8Way16 port map(
-		output0,
-		output1,
-		output2,
-		output3,
-		output4,
-		output5,
-		output6,
-		output7,
-		address,
-		output
-	);
 
 
 end architecture;
