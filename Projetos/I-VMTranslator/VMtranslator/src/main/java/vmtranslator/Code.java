384c2c8ea8410c409228963755eb31b935afe731
EricPossato <ericandreilimapossato@gmail.com>
2022-06-08 14:44:09 -0300

    SimplePopArg Implemented (não tem teste)

--- Projetos/I-VMTranslator/VMtranslator/src/main/java/vmtranslator/Code.java
+++ Projetos/I-VMTranslator/VMtranslator/src/main/java/vmtranslator/Code.java
@@ -141,6 +141,34 @@ public class Code {
             
 
             } else if (segment.equals("argument")) {
+                //atualiza SP
+                commands.add("leaw $0, %A");
+                commands.add("movw (%A), %D");
+                commands.add("decw %D");
+                commands.add("movw %D, (%A)");
+
+                //carrega valor do arg
+                commands.add("leaw $" + index + " %A");
+                commands.add("movw %A, %D");
+
+                //acessa endereço arg
+                commands.add("leaw $2, %A");
+                commands.add("movw (%A), %A");
+                commands.add("addw %A %D, %D");
+
+                //salvando endereço arg em temp0
+                commands.add("leaw $5, %A");
+                commands.add("movw %D, (%A)");
+
+                //Carrega em %D valor que quer salvar
+                commands.add("leaw $0, %A");
+                commands.add("movw (%A), %A");
+                commands.add("movw (%A), %D");
+                
+                //Armazena valor no arg desejado
+                commands.add("leaw $5, %A");
+                commands.add("movw (%A), %A");
+                commands.add("movw %D, (%A)");
 
             } else if (segment.equals("this")) {
 
