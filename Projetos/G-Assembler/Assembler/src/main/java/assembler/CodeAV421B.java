e417032594bfa8941df38cc6ccfd586e50521dac
ArthurCisotto <arthurcisottom@gmail.com>
2022-06-09 15:30:56 -0300

    assembler and vm questions from last test

---
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/CodeAV421B.java
@@ -0,0 +1,73 @@
+/**
+ * Curso: Elementos de Sistemas
+ * Arquivo: Code.java
+ */
+
+package assembler;
+
+/**
+ * Traduz mnemônicos da linguagem assembly para códigos binários da arquitetura Z0.
+ */
+public class CodeAV421B {
+
+    /**
+     * Retorna o código binário do(s) registrador(es) que serão carregados com o valor do leaw.
+     * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
+     * @return Opcode (String de 2 bits) com código em linguagem de máquina para a instrução.
+     *
+     * A função dest_load só será chamada para A_COMMAND
+     */
+    public static String dest_load(String[] mnemnonic) {
+        /* TODO: implementar */
+        if (mnemnonic.length > 3) {
+            return "11";
+        } else if (mnemnonic[2].equals("%A")) {
+            return "00";
+        } else if (mnemnonic[2].equals("%D")) {
+            return "01";
+        } else {
+            return "10";
+        }
+    }
+
+    /**
+     * Retorna o código binário do(s) registrador(es) que vão receber o valor da instrução.
+     * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
+     * @return Opcode (String de 4 bits) com código em linguagem de máquina para a instrução.
+     */
+    public static String dest(String[] mnemnonic) {
+        /* TODO: implementar */
+    	return "";
+    }
+
+    /**
+     * Retorna o código binário do mnemônico para realizar uma operação de cálculo.
+     * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
+     * @return Opcode (String de 7 bits) com código em linguagem de máquina para a instrução.
+     */
+    public static String comp(String[] mnemnonic) {
+        /* TODO: implementar */
+    	return "";
+    }
+
+    /**
+     * Retorna o código binário do mnemônico para realizar uma operação de jump (salto).
+     * @param  mnemnonic vetor de mnemônicos "instrução" a ser analisada.
+     * @return Opcode (String de 3 bits) com código em linguagem de máquina para a instrução.
+     */
+    public static String jump(String[] mnemnonic) {
+        /* TODO: implementar */
+    	return "";
+    }
+
+    /**
+     * Retorna o código binário de um valor decimal armazenado numa String.
+     * @param  symbol valor numérico decimal armazenado em uma String.
+     * @return Valor em binário (String de 15 bits) representado com 0s e 1s.
+     */
+    public static String toBinary(String symbol) {
+        /* TODO: implementar */
+    	return "";
+    }
+
+}
